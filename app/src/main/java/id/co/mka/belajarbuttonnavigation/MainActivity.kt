cd17031de1364ea7c8ffcefda68b076674fa3f85
iqbal <iqbalthamrin200@gmail.com>
2022-05-20 11:01:03 +0800

    integrasi api

--- app/src/main/java/id/co/mka/belajarbuttonnavigation/MainActivity.kt
+++ app/src/main/java/id/co/mka/belajarbuttonnavigation/MainActivity.kt
@@ -1,22 +1,21 @@
 package id.co.mka.belajarbuttonnavigation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import id.co.mka.belajarbuttonnavigation.data.entity.bestSellerEntity
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import id.co.mka.belajarbuttonnavigation.data.entity.kategoryEntity
 import id.co.mka.belajarbuttonnavigation.data.retrofit.RetrofitBuilder
 import id.co.mka.belajarbuttonnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -27,9 +26,17 @@ class MainActivity : AppCompatActivity() {
             viewModel.getCategory(RetrofitBuilder.getRetrofit(this@MainActivity.applicationContext))
                 .observe(this@MainActivity) {
 
-                    val adapter = cardViewAdapter(it as ArrayList<kategoryEntity>)
+                    val adapter = cardViewAdapter(it as ArrayList<kategoryEntity>) {
+//                        startActivity(
+//                            Intent(
+//                                this@MainActivity,
+//                                ?
+//                            )
+//                        )
+                    }
                     KategoryRecyclerView.adapter = adapter
-                    KategoryRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                    KategoryRecyclerView.layoutManager =
+                        StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
                 }
         }
 
--- app/src/main/java/id/co/mka/belajarbuttonnavigation/cardViewAdapter.kt
+++ app/src/main/java/id/co/mka/belajarbuttonnavigation/cardViewAdapter.kt
@@ -1,5 +1,6 @@
 package id.co.mka.belajarbuttonnavigation
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,21 +9,33 @@ import com.bumptech.glide.request.RequestOptions
 import id.co.mka.belajarbuttonnavigation.data.entity.kategoryEntity
 import id.co.mka.belajarbuttonnavigation.databinding.ItemKategoriBinding
 
-class cardViewAdapter (
-    private val listCategory : ArrayList<kategoryEntity>,
-        )
-        : RecyclerView.Adapter<cardViewAdapter.UserViewHolder>() {
-    inner class UserViewHolder(private val binding : ItemKategoriBinding) : RecyclerView.ViewHolder(binding.root) {
+class cardViewAdapter(
+    private val listCategory: ArrayList<kategoryEntity>,
+    private val userClickListener: (kategoryEntity) -> Unit
+) : RecyclerView.Adapter<cardViewAdapter.UserViewHolder>() {
+    inner class UserViewHolder(private val binding: ItemKategoriBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(kategoriItem: kategoryEntity) {
             with(binding) {
                 tvNameKategori.text = kategoriItem.category_name
+                kategoriItem.category_picture
+                Glide.with(imgCategori.context)
+                    .load("http://${kategoriItem.category_picture}")
+//                    .apply(RequestOptions().override(85, 85))
+                    .into(imgCategori)
+                Log.e("TAG", "bind: ${kategoriItem.category_picture}", )
+                root.setOnClickListener {
+                    userClickListener(kategoriItem)
+                }
 
             }
         }
     }
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): UserViewHolder {
-        val binding = ItemKategoriBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        val binding =
+            ItemKategoriBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
         return UserViewHolder(binding)
     }
 
@@ -31,7 +44,6 @@ class cardViewAdapter (
     }
 
     override fun getItemCount(): Int = listCategory.size
-
 
 
 }
